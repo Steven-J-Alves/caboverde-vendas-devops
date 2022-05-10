562a0f7b807b031d91d12d05dc2c433ddd5482d4
Steven Alves <steven.alves@advectus.net>
2022-05-10 13:16:34 -0100

    remove unecessary code

--- ansible/host-bootstrap.sh
+++ ansible/host-bootstrap.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 source venv/bin/activate 
-
 git restore bootstrap-aws-hosts/hosts 
-
 cd bootstrap-aws-hosts
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-
 ansible-playbook main.yaml -i hosts  
