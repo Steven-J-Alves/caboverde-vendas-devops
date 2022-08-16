2acb48eea94ad880f6749c02d78ceb7700635ddc
Steven Alves <steven.alves@advectus.net>
2022-08-16 21:56:56 -0100

    add env to show release version

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 
+const appVersion = process.env.NEXT_PUBLIC_APP_VERSION
+console.log("🚀 ~ file: footer.js ~ line 4 ~ appVersion", appVersion)
+
 export default ({ currentUser }) => {
   return (
     <footer className="navbar navbar-light footer">
       <p >Cabo Verde Vendas - Ecommerce</p>
       <p>2022</p>
+      <p>Release -  {appVersion}</p>
     </footer>
   );
 };
