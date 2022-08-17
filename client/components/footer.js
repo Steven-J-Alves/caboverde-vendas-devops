4aead79ed8354ef728de8c0c2bf2251b0dfd8655
Steven Alves <steven.alves@advectus.net>
2022-08-17 21:56:33 -0100

    update client verison

--- client/components/footer.js
+++ client/components/footer.js
@@ -8,7 +8,7 @@ export default ({ currentUser }) => {
     <footer className="navbar navbar-light footer">
       <p >Cabo Verde Vendas - Ecommerce</p>
       <p>2022</p>
-      <p>Release -  {appVersion}</p>
+      <p>Release -  v2.7.4</p>
     </footer>
   );
 };
