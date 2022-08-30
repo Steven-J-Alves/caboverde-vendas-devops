94b7d2fefb6b31659109d011cced53c56ddc97d5
Steven Alves <steven.alves@advectus.net>
2022-08-30 13:21:53 -0100

    add deploy infra env

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
 
+const deploy_infra = process.env.DEPLOY_INFRA
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
-
-    return axios.create({
-      baseURL: 'http://www.caboverde-vendas-prod.xyz/',
-      headers: req.headers,
-    });
+    if (deploy_infra === "staging") {
+      return axios.create({
+        baseURL: 'http://staging.caboverde-vendas-prod.xyz/',
+        headers: req.headers,
+      });
+    } else if (deploy_infra === "production") {
+      return axios.create({
+        baseURL: 'http://www.caboverde-vendas-prod.xyz/',
+        headers: req.headers,
+      });
+    } else {
+      return axios.create({
+        baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
+        headers: req.headers,
+      });
+    }
   } else {
-    // We must be on the browser
     return axios.create({
       baseUrl: '/',
     });
   }
 };
-
-// baseURL:  'http://www.caboverde-vendas-prod.xyz/',
-// baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
