09d3e34669a7c81939c04487c0f1f84111f8d421
Steven Alves <steven.alves@advectus.net>
2022-08-30 13:30:00 -0100

    fix comments

--- app-bootstrap.sh
+++ app-bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# digitalOcean setup k8s
 kubectl create namespace staging  
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/do/deploy.yaml
