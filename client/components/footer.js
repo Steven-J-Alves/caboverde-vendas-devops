a4ba3b65ce556bc8513a1d961bda8688802f0a6a
Steven Alves <steven.alves@advectus.net>
2022-12-13 10:29:49 -0100

    CVP-1312 add tag version and change paragrahp text

--- client/components/footer.js
+++ client/components/footer.js
@@ -2,9 +2,9 @@ export default () => {
   return (
     <footer className="navbar navbar-light footer">
       <p >Cabo Verde Vendas - Ecommerce</p>
-      <p >New resources available for TCC hands on!</p>
+      <p >New features added for TCC hands on!</p>
       <p>2022</p>
-      <p>Release -  v5.0.0</p>
+      <p>Release -  V6.0.0</p>
     </footer>
   );
 };
